293d65788202e4a01a402584c09f5f15d004e80a
Bence Ferdinandy <bence@ferdinandy.com>
2022-01-12 18:13:59 +0100

    often used alias

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -8,3 +8,4 @@ alias jn="jupyter notebook"
 alias ts="$HOME/.tmux/scripts/fzf-change-session.tmux tmux_fzf_change_session"
 alias tn="$HOME/.tmux/scripts/fzf-change-session.tmux tmux_fzf_new_session"
 alias r='printf "\033];Ranger\007"; ranger; printf "\033];Terminal\007"'
+alias lsr="ls -tr"

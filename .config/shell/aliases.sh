3274e76018f2cdde044e5992fd86c38e65c631e7
Bence Ferdinandy <bence@ferdinandy.com>
2022-06-26 22:00:11 +0200

    fix alias

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -7,7 +7,7 @@ alias tns="tmux new -s"
 alias jn="jupyter notebook"
 alias ts="$HOME/.tmux/scripts/fzf-change-session.tmux tmux_fzf_change_session"
 alias tn="$HOME/.tmux/scripts/fzf-change-session.tmux tmux_fzf_new_session"
-alias r='printf "\033];Ranger\007"; ranger; printf "\033];Terminal\007"'
+alias r='. ranger'
 alias lsr="ls -tr"
 
 # cetli.vim

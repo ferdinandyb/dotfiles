5b25d49d906bbd5fba77e6ba0d534acbab29340d
Bence Ferdinandy <bence@ferdinandy.com>
2023-08-29 12:07:21 +0200

    ipython: first look at configs

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,16 @@
+c = get_config()  # noqa
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalIPythonApp.display_banner = False
+# c.TerminalInteractiveShell.shortcuts = [
+#     {
+#         "command": "IPython:auto_suggest.accept",
+#         "new_keys": ["c-z"],
+#     }
+# ]
+## Shortcut style to use at the prompt. 'vi' or 'emacs'.
+#  Default: 'emacs'
+# c.TerminalInteractiveShell.editing_mode = 'emacs'
+
+## Set the editor used by IPython (default to $EDITOR/vi/notepad).
+#  Default: 'vim'
+# c.TerminalInteractiveShell.editor = 'vim'

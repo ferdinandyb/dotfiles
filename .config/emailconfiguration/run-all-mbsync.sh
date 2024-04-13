55a664b2530eb8e0ea7ee4a7fb75acf35756a13d
Bence Ferdinandy <bence@ferdinandy.com>
2024-04-13 22:45:15 +0200

    throttle: simplify path in script

--- .config/emailconfiguration/run-all-mbsync.sh
+++ .config/emailconfiguration/run-all-mbsync.sh
@@ -7,13 +7,11 @@ if [ -z "$1" ]
     origin=$1
 fi
 
-throttlepath=$HOME/.local/bin/throttle
-
 # gathers all the channels in isyncrc
 channels=$(grep '^Channel' $HOME/.config/isyncrc | sort | uniq | sed 's/Channel //')
 # run the machine dependent channels
 case $(hostname) in
-  ("mashenka") echo "$channels" | grep -v ^yettel | xargs -i $throttlepath --origin $origin --silent-job "testinternetconnection" --job "mbsync {}" --job "notmuch new";;
-  ("BFERDINANDY-NB") echo "$channels" | grep ^yettel | xargs -i $throttlepath --origin $origin --silent-job "testinternetconnection" --job "mbsync {}" --job "notmuch new";;
+  ("mashenka") echo "$channels" | grep -v ^yettel | xargs -i throttle --origin $origin --silent-job "testinternetconnection" --job "mbsync {}" --job "notmuch new";;
+  ("BFERDINANDY-NB") echo "$channels" | grep ^yettel | xargs -i throttle --origin $origin --silent-job "testinternetconnection" --job "mbsync {}" --job "notmuch new";;
   (*)   echo "not on known machine";;
 esac

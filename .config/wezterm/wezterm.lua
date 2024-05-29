0b165806bd5cd9b10043abd6ae3f03c0819ef3b2
Bence Ferdinandy <bence@ferdinandy.com>
2024-05-29 22:04:11 +0200

    wezterm: getting to a tmuxless config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,11 +17,10 @@ config.color_scheme = 'Dracula'
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   config.default_prog = { 'wsl', '--cd', '~' }
 end
+-- config.leader = { key = 'space', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.window_close_confirmation = 'NeverPrompt'
 -- config.disable_default_key_bindings = true
 config.keys = {
-  -- Turn off the default CMD-m Hide action, allowing CMD-m to
-  -- be potentially recognized and handled by the tab
   {
     key = 'V',
     mods = 'CTRL',
@@ -33,15 +32,45 @@ config.keys = {
     action = wezterm.action.CopyTo 'Clipboard'
   },
   {
-    key = '0',
+    key = 'mapped:§',
     mods = 'CTRL|SHIFT',
     action = wezterm.action.ActivateTab(0)
   },
   {
-    key = '1',
+    key = "mapped:'",
     mods = 'CTRL|SHIFT',
     action = wezterm.action.ActivateTab(1)
-  }
+  },
+  {
+    key = 'mapped:Ü',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'mapped:Ö',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'Z',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.TogglePaneZoomState,
+  },
+  {
+    key = 's',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' },
+  },
+  {
+    key = 'n',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SwitchToWorkspace
+  },
+  {
+    key = 'd',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DetachDomain 'CurrentPaneDomain',
+  },
 }
 config.check_for_updates = false
 config.hide_tab_bar_if_only_one_tab = true
@@ -50,4 +79,109 @@ config.font_size = 13
 config.unicode_version = 15
 
 config.font = wezterm.font('CaskaydiaCove Nerd Font', { weight = 'Light' })
+config.ssh_domains = {
+  {
+    -- This name identifies the domain
+    name = 'wsl.local',
+    -- The hostname or address to connect to. Will be used to match settings
+    -- from your ssh config file
+    remote_address = 'localhost:2222',
+    -- The username to use on the remote host
+    username = 'bferdinandy',
+  },
+}
+
+
+wezterm.on('update-right-status', function(window, pane)
+  -- Each element holds the text for a cell in a "powerline" style << fade
+  local cells = {}
+
+  -- Figure out the cwd and host of the current pane.
+  -- This will pick up the hostname for the remote host if your
+  -- shell is using OSC 7 on the remote host.
+  local cwd_uri = pane:get_current_working_dir()
+  if cwd_uri then
+    local cwd = ''
+    local hostname = ''
+
+    if type(cwd_uri) == 'userdata' then
+      -- Running on a newer version of wezterm and we have
+      -- a URL object here, making this simple!
+
+      cwd = cwd_uri.file_path
+      hostname = cwd_uri.host or wezterm.hostname()
+    else
+      -- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+      -- which doesn't have the Url object
+      cwd_uri = cwd_uri:sub(8)
+      local slash = cwd_uri:find '/'
+      if slash then
+        hostname = cwd_uri:sub(1, slash - 1)
+        -- and extract the cwd from the uri, decoding %-encoding
+        cwd = cwd_uri:sub(slash):gsub('%%(%x%x)', function(hex)
+          return string.char(tonumber(hex, 16))
+        end)
+      end
+    end
+
+    -- Remove the domain name portion of the hostname
+    local dot = hostname:find '[.]'
+    if dot then
+      hostname = hostname:sub(1, dot - 1)
+    end
+    if hostname == '' then
+      hostname = wezterm.hostname()
+    end
+
+    table.insert(cells, window:active_workspace())
+    table.insert(cells, hostname)
+  end
+
+  -- I like my date/time in this style: "Wed Mar 3 08:14"
+  local date = wezterm.strftime '%a %b %-d %H:%M'
+  table.insert(cells, date)
+
+
+  -- The powerline < symbol
+  local LEFT_ARROW = utf8.char(0xe0b3)
+  -- The filled in variant of the < symbol
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+  -- Color palette for the backgrounds of each cell
+  local colors = {
+    '#3c1361',
+    '#52307c',
+    '#663a82',
+    '#7c5295',
+    '#b491c8',
+  }
+
+  -- Foreground color for the text across the fade
+  local text_fg = '#c0c0c0'
+
+  -- The elements to be formatted
+  local elements = {}
+  -- How many cells have been formatted
+  local num_cells = 0
+
+  -- Translate a cell into elements
+  function push(text, is_last)
+    local cell_no = num_cells + 1
+    table.insert(elements, { Foreground = { Color = text_fg } })
+    table.insert(elements, { Background = { Color = colors[cell_no] } })
+    table.insert(elements, { Text = ' ' .. text .. ' ' })
+    if not is_last then
+      table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+      table.insert(elements, { Text = SOLID_LEFT_ARROW })
+    end
+    num_cells = num_cells + 1
+  end
+
+  while #cells > 0 do
+    local cell = table.remove(cells, 1)
+    push(cell, #cells == 0)
+  end
+
+  window:set_right_status(wezterm.format(elements))
+end)
 return config

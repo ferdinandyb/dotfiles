66097d0d1c4a6183f7fc34b6277bd1a877f8b33c
Bence Ferdinandy <bence@ferdinandy.com>
2025-07-25 13:43:15 +0200

    email: no need for previous company stuff
    Signed-off-by: Bence Ferdinandy <bence@ferdinandy.com>

--- .config/emailconfiguration/run-all-mbsync.sh
+++ .config/emailconfiguration/run-all-mbsync.sh
@@ -10,8 +10,4 @@ fi
 # gathers all the channels in isyncrc
 channels=$(grep '^Channel' $HOME/.config/isyncrc | sort | uniq | sed 's/Channel //')
 # run the machine dependent channels
-case $(hostname) in
-  ("mashenka") echo "$channels" | grep -v ^yettel | xargs -i throttle --origin $origin --silent-job "testinternetconnection" --job "mbsync {}" --job "notmuch new";;
-  ("BFERDINANDY-NB") echo "$channels" | grep ^yettel | xargs -i throttle --origin $origin --silent-job "testinternetconnection" --job "mbsync {}" --job "notmuch new";;
-  (*)   echo "not on known machine";;
-esac
+echo "$channels" | xargs -i throttle --origin $origin --silent-job "testinternetconnection" --job "mbsync {}" --job "notmuch new"

aa11695b2695b1f1acb0419912c719183705bf37
Bence Ferdinandy <bence@ferdinandy.com>
2025-07-26 22:51:13 +0200

    tasks: improve syncing
    Signed-off-by: Bence Ferdinandy <bence@ferdinandy.com>

---
+++ .config/systemd/user/tasksync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+task sync
+
+tw_caldav_sync --caldav-url https://nc.vpn.ferdinandy.com/remote.php/dav/calendars/bence --caldav-calendar tw --caldav-user bence --caldav-passwd nextcloud/bence --all

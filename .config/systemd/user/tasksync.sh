9a758c3c53ac651472c6c7c2f23f4247355f90a5
Bence Ferdinandy <bence@ferdinandy.com>
2025-08-01 12:30:23 +0200

    tasksync: add homebrew if it exists
    On Ubuntu it's a pain to have certain packages up-to-date, hence
    homebrew, but that needs to be eval-ed. Aur is a bit better than
    homebrew since it has more packages and is seamless of course, not like
    this eval, but on the downside, aur updates _everything_ which is
    another kind of pain.
    
    Signed-off-by: Bence Ferdinandy <bence@ferdinandy.com>

--- .config/systemd/user/tasksync.sh
+++ .config/systemd/user/tasksync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 task sync
-
 tw_caldav_sync --caldav-url https://nc.vpn.ferdinandy.com/remote.php/dav/calendars/bence --caldav-calendar tw --caldav-user bence --caldav-passwd nextcloud/bence --all

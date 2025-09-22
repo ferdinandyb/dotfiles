55879c1aa621d014a7eb9544783fff8feec7f282
Bence Ferdinandy <bence@ferdinandy.com>
2025-09-22 23:18:39 +0200

    tmux: fix confed resurrect
    It turns out that the alias didn't work, because the command was
    actually vim running. But the shell script confed didn't work either,
    because the actual process was `/bin/sh confed`. So we need the regex
    match.

--- .config/shell/zsh/customfunctions.zsh
+++ .config/shell/zsh/customfunctions.zsh
@@ -2,13 +2,13 @@ function myissues(){
   jira issue list -q "assignee = currentUser() AND status NOT IN ('Done', 'Closed', 'Resolved', 'Won\'t Do')"
 }
 
-function confed(){
-  env GIT_DIR=$HOME/.local/share/yadm/repo.git GIT_WORK_TREE=$HOME \
-  vim -c "cd ~" \
-      -c "let g:rooter_change_directory_for_non_project_files = 'home'" \
-      -c "silent AutoSaveToggle" \
-      -S ~/.local/share/yadm/Session.vim
-}
+# function confed(){
+#   env GIT_DIR=$HOME/.local/share/yadm/repo.git GIT_WORK_TREE=$HOME \
+#   vim -c "cd ~" \
+#       -c "let g:rooter_change_directory_for_non_project_files = 'home'" \
+#       -c "silent AutoSaveToggle" \
+#       -S ~/.local/share/yadm/Session.vim
+# }
 
 function y() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd

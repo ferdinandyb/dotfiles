6bd01b06a653e7f68d63c4d53d308a02f0ce27f2
Bence Ferdinandy <bence@ferdinandy.com>
2026-06-08 23:10:51 +0200

    restic: cleanup

--- .restic.sh
+++ .restic.sh
@@ -3,75 +3,49 @@
 HDDPATH=/run/media/fbence/LaCie
 RCLONE=rclone:pharmahungarybence:mashenka-restic
 
+export RESTIC_PASSWORD_COMMAND="pass show restic"
+
+FORGET_OPTS=(
+    --keep-within-daily 7d
+    --keep-within-weekly 1m
+    --keep-within-monthly 1y
+    --keep-within-yearly 75y
+)
+
+run_backup() {
+    local backend=$1; shift
+    echo "backend: $backend"
+    restic backup -r "$backend" "$@"
+    restic forget -r "$backend" "${FORGET_OPTS[@]}"
+}
+
+run_named_backup() {
+    local name=$1; shift
+    run_backup "local:$HDDPATH/restic/$name" "$@"
+    # run_backup "$RCLONE/$name"               "$@"
+}
+
 echo email
-for backend in \
-    local:$HDDPATH/restic/email \
-    $RCLONE/email
-do
-    echo backend: $backend
-    restic backup -r $backend --password-command "pass show restic" \
-        /home/fbence/.mail \
-        --exclude=".notmuch/**"
-
-    restic forget -r $backend --password-command "pass show restic" \
-        --keep-within-daily 7d   \
-        --keep-within-weekly 1m  \
-        --keep-within-monthly 1y \
-        --keep-within-yearly 75y
-
-done
+run_named_backup email \
+    /home/fbence/.mail \
+    --exclude=".notmuch/**"
 
 echo documents
-for backend in \
-    local:$HDDPATH/restic/documents \
-    $RCLONE/documents
-do
-    echo backend: $backend
-    restic backup -r $backend --password-command "pass show restic" \
-        /home/fbence/Documents
-
-    restic forget -r $backend --password-command "pass show restic" \
-        --keep-within-daily 7d   \
-        --keep-within-weekly 1m  \
-        --keep-within-monthly 1y \
-        --keep-within-yearly 75y
-
-done
+run_named_backup documents \
+    /home/fbence/Documents
 
 echo pictures
-for backend in \
-    local:$HDDPATH/restic/pictures \
-    $RCLONE/pictures
-do
-    echo backend: $backend
-    restic backup -r $backend --password-command "pass show restic" \
-        /disk2/Pictures \
-        /disk2/FamilyPictures \
-        /disk2/PicturesÉviBence \
-        /disk2/PhonePics
-
-    restic forget -r $backend --password-command "pass show restic" \
-        --keep-within-daily 7d   \
-        --keep-within-weekly 1m  \
-        --keep-within-monthly 1y \
-        --keep-within-yearly 75y
-
-done
+run_named_backup pictures \
+    /disk2/Pictures \
+    /disk2/FamilyPictures \
+    /disk2/PicturesÉviBence \
+    /disk2/PhonePics
 
 echo org
-for backend in \
-    local:$HDDPATH/restic/org \
-    $RCLONE/org
-do
-    echo backend: $backend
-    restic backup -r $backend --password-command "pass show restic" \
-        /home/fbence/org \
-        /home/fbence/.task
+run_named_backup org \
+    /home/fbence/org \
+    /home/fbence/.task
 
-    restic forget -r $backend --password-command "pass show restic" \
-        --keep-within-daily 7d   \
-        --keep-within-weekly 1m  \
-        --keep-within-monthly 1y \
-        --keep-within-yearly 75y
-
-done
+echo gdrive
+run_named_backup gdrive \
+    /disk2/gdrive
